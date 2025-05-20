2c540da235a42f4f659efd4dcb49e33250571bf6
BDanl <alejandrobovea@gmail.com>
2025-05-20 13:54:12 -0500

    flashfix

--- App.js
+++ App.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React from "react";
 import { Alert, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import MainStackNavigator from "./Navigation/MainStackNavigator";
 import { UsersProvider } from "./context/UsersContext";
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { RoutinesProvider } from "./context/RoutinesContext";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <RoutinesProvider>
-      <UsersProvider>
-        <NavigationContainer>
-          <SafeAreaView style={styles.container}>
-            <MainStackNavigator />
-          </SafeAreaView>
-        </NavigationContainer>
-      </UsersProvider>
+        <UsersProvider>
+          <NavigationContainer
+            theme={{
+              colors: {
+                background: "black", // o el color de tu app
+              },
+            }}
+          >
+            <SafeAreaView style={styles.container}>
+              <MainStackNavigator />
+            </SafeAreaView>
+          </NavigationContainer>
+        </UsersProvider>
       </RoutinesProvider>
     </GestureHandlerRootView>
   );
