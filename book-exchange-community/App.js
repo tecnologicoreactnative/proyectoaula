a237f850d6841b244692b7ef5baad4065182f260
Milo2404 <andres.diaz95@correo.tdea.edu.co>
2025-05-20 14:45:14 -0500

    Perfil listo

--- book-exchange-community/App.js
+++ book-exchange-community/App.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import {NavigationContainer} from "@react-navigation/native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 
-import {AppProvider} from "./context/AppContext";
-import AppNavigation from './navigation/AppNavigation';
-import {SafeAreaProvider} from "react-native-safe-area-context";
+import { AppProvider } from "./context/AppContext";
+import AppNavigation from "./navigation/AppNavigation";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
-    return (
-        <AppProvider>
-            <SafeAreaProvider>
-                <NavigationContainer>
-                    <AppNavigation/>
-                </NavigationContainer>
-            </SafeAreaProvider>
-        </AppProvider>
-    );
+  return (
+    <AppProvider>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <AppNavigation />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </AppProvider>
+  );
 }
-
-
