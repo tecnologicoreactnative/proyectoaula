9b86a59c43d6fc7be96e854567651e9c7446787a
BDanl <alejandrobovea@gmail.com>
2025-05-20 13:54:38 -0500

    clean

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
           <NavigationContainer
             theme={{
               colors: {
-                background: "black", // o el color de tu app
+                background: "black",
               },
             }}
           >
